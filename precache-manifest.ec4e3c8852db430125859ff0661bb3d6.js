8d4a374826cea16bf0fe3708cf54b7c4e8523bf9
Valeria Gora <valeriia308@gmail.com>
2019-05-08 22:07:31 +0300

    Updates

---
+++ precache-manifest.ec4e3c8852db430125859ff0661bb3d6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "578fcaf9f8ded3f6371a7a0d21de1545",
+    "url": "/test_task/index.html"
+  },
+  {
+    "revision": "db54418993287e022d14",
+    "url": "/test_task/static/css/main.efe73508.chunk.css"
+  },
+  {
+    "revision": "de12574d7feb2744e0de",
+    "url": "/test_task/static/js/2.999e4f62.chunk.js"
+  },
+  {
+    "revision": "db54418993287e022d14",
+    "url": "/test_task/static/js/main.b9fb2d71.chunk.js"
+  },
+  {
+    "revision": "ad17ed891eb3ecd9a451",
+    "url": "/test_task/static/js/runtime~main.ef32a091.js"
+  }
+]);
